6e43508c64909de35b111bf5fbdd314f08066495
ben-scully <ben@moregolf.co.nz>
2017-08-03 22:24:01 +1200

    Added very rudimentary tests

--- atbash.js
+++ atbash.js
@@ -14,12 +14,6 @@ Add test/spec to your solution.
 
 
 
-var constant = 'abcdefghijklmnopqrstuvwxyz'
-var cipher =   'oephjizkxdawubnytvfglqsrcm'
-var encryptedText = 'knlfgnb, sj koqj o yvnewju'
-var originalText  = '';
-
-
 function getDecrypter(constant, cipher) {
   var decrypter = {}
 
@@ -51,6 +45,37 @@ function decrypt(encryptedText, decrypter) {
 
 
 
-var decrypter = getDecrypter(constant, cipher)
+var constant1 =      'abcdefghijklmnopqrstuvwxyz'
+var cipher1 =        'zodvqukgwefbyitmrsplhacxnj'
+var encryptedText1 = 'dzs'
+var originalText1  = 'car'
 
-console.log(decrypt(encryptedText, decrypter))
+var constant2 =       'abcdefghijklmnopqrstuvwxyz'
+var cipher2 =         'xipmuzfkbrlwotjancqgveshdy'
+var encryptedText2 =  'skd qj qucbjvq?'
+var originalText2 =   'why so serious?'
+
+var constant3 =       'abcdefghijklmnopqrstuvwxyz'
+var cipher3 =         'oephjizkxdawubnytvfglqsrcm'
+var encryptedText3 =  'knlfgnb, sj koqj o yvnewju'
+var originalText3  =  '?'
+
+
+
+var decrypter1 = getDecrypter(constant1, cipher1)
+var decryptedText1 = decrypt(encryptedText1, decrypter1)
+console.log("Test1 -> original text: " + originalText1 + " decrypted text: " + decryptedText1)
+console.log("Test passed?: " + (decryptedText1 == originalText1))
+
+
+
+var decrypter2 = getDecrypter(constant2, cipher2)
+var decryptedText2 = decrypt(encryptedText2, decrypter2)
+console.log("Test1 -> original text: " + originalText2 + " decrypted text: " + decryptedText2)
+console.log("Test passed?: " + (decryptedText2 == originalText2))
+
+
+
+var decrypter3 = getDecrypter(constant3, cipher3)
+var decryptedText3 = decrypt(encryptedText3, decrypter3)
+console.log("Answer to \"The Challenge\": ", decryptedText3)
